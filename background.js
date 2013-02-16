32f82ef850a650ffdfcc8142663cd727910e4236
Petar Bojinov <petarbojinov@gmail.com>
2013-02-15 16:58:39 -0800

    fix persistence bug
    press on icon brings to app, only one instance can be open at a time

--- background.js
+++ background.js
@@ -2,29 +2,35 @@
  * Author: Petar
  * Date: 2/15/13
  */
-var win;
+var winId;
 var drawAttention = false;
 
 chrome.browserAction.onClicked.addListener(function() {
 
-    if (!win) {
+    //alert(win);
+
+    if (!winId) {
         var w = 656,
             h = 515,
             focus = true;
 
         chrome.windows.create({'url': 'discover.html', 'type': 'detached_panel', 'focused' : focus, 'width' : w, 'height' : h},
             function(window) {
-                win = window;
-                win.id = window.id;
+                winId = window.id;
             });
     }
     else {
 
-        chrome.windows.update(win.id, {'focused' : true, 'drawAttention' : drawAttention},
+        chrome.windows.update(winId, {'focused' : true, 'drawAttention' : drawAttention},
             function(window) {
         });
     }
 
 /*    chrome.windows.create({'url': 'discover.html', 'type': 'popup'}, function(window) {
     });*/
+});
+
+chrome.windows.onRemoved.addListener(function(windowId) {
+    //alert('closed ' + windowId);
+    winId = undefined;
 });
