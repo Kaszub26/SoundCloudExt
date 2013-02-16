9090de07e262cdfa7410d0f433cf279207b99f64
Petar Bojinov <petarbojinov@gmail.com>
2013-02-15 16:36:20 -0800

    add player persistence, update crx
    only one instance is open

--- background.js
+++ background.js
@@ -2,17 +2,28 @@
  * Author: Petar
  * Date: 2/15/13
  */
+var win;
+var drawAttention = false;
+
 chrome.browserAction.onClicked.addListener(function() {
-    var w = 656,
-        h = 515,
- /*       left = (screen.width/2) - (w/2),
-        top = (screen.height/2) - (h/2),*/
-        focus = true;
 
-    chrome.windows.create({'url': 'discover.html', 'type': 'detached_panel', 'focused' : focus, 'width' : w, 'height' : h},
-        function(window) {
+    if (!win) {
+        var w = 656,
+            h = 515,
+            focus = true;
 
-    });
+        chrome.windows.create({'url': 'discover.html', 'type': 'detached_panel', 'focused' : focus, 'width' : w, 'height' : h},
+            function(window) {
+                win = window;
+                win.id = window.id;
+            });
+    }
+    else {
+
+        chrome.windows.update(win.id, {'focused' : true, 'drawAttention' : drawAttention},
+            function(window) {
+        });
+    }
 
 /*    chrome.windows.create({'url': 'discover.html', 'type': 'popup'}, function(window) {
     });*/
